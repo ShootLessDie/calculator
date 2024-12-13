17555654954ca16452e46639dc7dc259269a9330
Gelimester <prboiii@gmail.com>
2024-12-13 18:19:50 +0000

    finished input and added prettier config

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import Input from './calculator/Input';
-import ResultDisplay from './calculator/ResultDisplay';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import Input from "./calculator/Input";
+import ResultDisplay from "./calculator/ResultDisplay";
 
 export default function App() {
   return (
     <PaperProvider>
-      <StatusBar style="inverted" translucent={false}/>
+      <StatusBar style="inverted" translucent={false} />
       <View style={styles.container}>
-          <ResultDisplay />
-          <Input />
-        </View>
+        <ResultDisplay />
+        <Input />
+      </View>
     </PaperProvider>
   );
 }
@@ -19,13 +19,13 @@ export default function App() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
   container: {
     flex: 1,
     backgroundColor: "#fdf6f4",
-    width: "100%"
-  }
+    width: "100%",
+  },
 });
--- calculator/ResultDisplay.tsx
+++ calculator/ResultDisplay.tsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-paper'
-import { Dimensions } from 'react-native'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
 
 const ResultDisplay = () => {
-
   return (
-    <View style={{...styles.container}}>
+    <View style={{ ...styles.container }}>
       <Text>ResultDisplay</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:"#eedfd4",
+  container: {
+    backgroundColor: "#eedfd4",
     borderBottomLeftRadius: 40,
     borderBottomRightRadius: 40,
-    flex:1
+    flex: 1,
   },
-  row:{
-    flexDirection:'row'
+  row: {
+    flexDirection: "row",
   },
   item: {
-    flexGrow:1,
-    alignItems: 'center',
-    justifyContent: 'center', 
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
-
 });
 
-export default ResultDisplay
+export default ResultDisplay;
